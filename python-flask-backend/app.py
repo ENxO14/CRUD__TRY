db2f6941523833b6f9ef435acc635dd5fbeb1f9c
Sasmitha Livera <livera.sasmitha@iisgalvanimi.edu.it>
2022-05-06 21:28:48 +0000

    ciao

---
+++ python-flask-backend/app.py
@@ -0,0 +1,106 @@
+from flask import Flask, render_template, request, jsonify
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = 'mongodb://Livera2003:Waduge78&@cluster0-shard-00-00.uylmr.mongodb.net:27017,cluster0-shard-00-01.uylmr.mongodb.net:27017,cluster0-shard-00-02.uylmr.mongodb.net:27017/Livera1?ssl=true&replicaSet=atlas-38dmxu-shard-0&authSource=admin&retryWrites=true&w=majority'
+# db = client.lin_flask
+mongo = PyMongo(app)
+CORS(app)
+
+@app.route('/')
+def index():
+    return "Ciao"
+
+@app.route('/users', methods=['POST', 'GET'])
+def data():
+    
+    # POST a data to database
+    if request.method == 'POST':
+        body = request.json
+        firstName = body['firstName']
+        lastName = body['lastName']
+        emailId = body['emailId'] 
+        # db.users.insert_one({
+        mongo.db.appuntamenti.insert_one({
+            "firstName": firstName,
+            "lastName": lastName,
+            "emailId":emailId
+        })
+        return jsonify({
+            'status': 'Data is posted to MongoDB!',
+            'firstName': firstName,
+            'lastName': lastName,
+            'emailId':emailId
+        })
+    
+    # GET all data from database
+    if request.method == 'GET':
+        allData =  mongo.db.appuntamenti.find()
+        dataJson = []
+        for data in allData:
+            id = data['_id']
+            firstName = data['firstName']
+            lastName = data['lastName']
+            emailId = data['emailId']
+            dataDict = {
+                'id': str(id),
+                'firstName': firstName,
+                'lastName': lastName,
+                'emailId': emailId
+            }
+            dataJson.append(dataDict)
+        print(dataJson)
+        return jsonify(dataJson)
+
+@app.route('/users/<string:id>', methods=['GET', 'DELETE', 'PUT'])
+def onedata(id):
+
+    # GET a specific data by id
+    if request.method == 'GET':
+        data = mongo.db.appuntamenti.find_one({'_id': ObjectId(id)})
+        id = data['_id']
+        firstName = data['firstName']
+        lastName = data['lastName']
+        emailId = data['emailId']
+        dataDict = {
+            'id': str(id),
+            'firstName': firstName,
+            'lastName': lastName,
+            'emailId':emailId
+        }
+        print(dataDict)
+        return jsonify(dataDict)
+        
+    # DELETE a data
+    if request.method == 'DELETE':
+        mongo.db.appuntamenti.delete_many({'_id': ObjectId(id)})
+        print('\n # Deletion successful # \n')
+        return jsonify({'status': 'Data id: ' + id + ' is deleted!'})
+
+    # UPDATE a data by id
+    if request.method == 'PUT':
+        body = request.json
+        firstName = body['firstName']
+        lastName = body['lastName']
+        emailId = body['emailId']
+
+        db['users'].update_one(
+            {'_id': ObjectId(id)},
+            {
+                "$set": {
+                    "firstName":firstName,
+                    "lastName":lastName,
+                    "emailId": emailId
+                }
+            }
+        )
+
+        print('\n # Update successful # \n')
+        return jsonify({'status': 'Data id: ' + id + ' is updated!'})
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
